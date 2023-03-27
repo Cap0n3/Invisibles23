df9ab091d8ef45a028b879d305a0e49f43429da2
Cap0n3 <cap0n3@protonmail.com>
2023-03-27 11:43:58 +0200

    Added hero header & dynamic navbar

---
+++ Invisibles23/website/static/website/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", event => {
+    const scrollPos = document.documentElement.scrollTop
+    const nav = document.getElementsByClassName("navbar")
+    
+    if(scrollPos >= 70) {
+        nav.item(0).classList.add("show")
+    }
+    else {
+        nav.item(0).classList.remove("show")
+    }  
+}, {passive: true})
