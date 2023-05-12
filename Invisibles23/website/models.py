0bc62844aae20b014dfd5ddb6b4cd86a874a29fb
Cap0n3 <cap0n3@protonmail.com>
2023-05-12 19:48:17 +0200

    Added images to sections

--- Invisibles23/website/models.py
+++ Invisibles23/website/models.py
@@ -6,7 +6,7 @@ class WebpageSection (models.Model):
     title = models.CharField(max_length=50) # Actual title of the section
     text = models.TextField(max_length=10000)
     custom_html = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='')
 
     def __str__(self):
         return self.title
