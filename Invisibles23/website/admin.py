0e935adbfdd7f7f046823c05762dfed39d94b478
Cap0n3 <cap0n3@protonmail.com>
2023-05-12 15:39:43 +0200

    Started to work on sections

--- Invisibles23/website/admin.py
+++ Invisibles23/website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import WebpageSection, Paragraphs
 
 # Register your models here.
+admin.site.register(WebpageSection)
+admin.site.register(Paragraphs)
--- Invisibles23/website/models.py
+++ Invisibles23/website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class WebpageSection (models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
+    
+class Paragraphs (models.Model):
+    section = models.ForeignKey(WebpageSection, on_delete=models.CASCADE)
+    paragraph = models.TextField()
+
+    def __str__(self):
+        return self.paragraph
