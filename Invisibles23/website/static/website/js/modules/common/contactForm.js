03e33c2d55879a155ba66110f815efddbca66c57
Cap0n3 <cap0n3@protonmail.com>
2023-05-16 16:36:49 +0200

    Starting js client-side validation

--- Invisibles23/website/static/website/js/modules/common/contactForm.js
+++ Invisibles23/website/static/website/js/modules/common/contactForm.js
@@ -1,19 +1,19 @@
 export function contactForm() {
-    'use strict'
+    // follow doc : https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+    const form = document.getElementById("contactForm");
+    const email = document.getElementById("id_email");
 
-    const forms = document.querySelectorAll('.contactForm');
-
-    // Create an array and iterate over each element to add an event listener
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-            // Add bootstrap 4 was-validated classes to trigger validation messages
-            form.classList.add('was-validated')
-        }, false)
-    })
-
-
+    email.addEventListener("input", (event) => {
+        // Each time the user types something, we check if the
+        // form fields are valid.
+        
+        if (email.validity.valid) {
+            // In case there is an error message visible, if the field
+            // is valid, we remove the error message.
+            console.log("Not valid")
+        } else {
+            // If there is still an error, show the correct error
+            console.log("Not valid")
+        }
+    });
 }
