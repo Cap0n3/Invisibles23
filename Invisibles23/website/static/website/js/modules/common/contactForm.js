586729ed9c5027ec68ceaa6a24370efe928d897d
Cap0n3 <cap0n3@protonmail.com>
2023-05-16 20:18:53 +0200

    Improve client-side verif code (still in dev)

--- Invisibles23/website/static/website/js/modules/common/contactForm.js
+++ Invisibles23/website/static/website/js/modules/common/contactForm.js
@@ -1,20 +1,28 @@
 export function contactForm() {
     // follow doc : https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
     const form = document.getElementById("contactForm");
+    const fname = document.getElementById("id_first_name");
+    const lname = document.getElementById("id_last_name");
     const email = document.getElementById("id_email");
 
     email.addEventListener("input", (event) => {
-        // Each time the user types something, we check if the
-        // form fields are valid.
-        
-        if (email.validity.valid) {
-            console.log("Valid")
-            email.classList.remove('is-invalid') // if it was invalid before
-            email.classList.add('is-valid')
-
-        } else {
-            console.log("Not valid")
-            email.classList.add('is-invalid')
-        }
+        validateEmail(email);
     });
+}
+
+function validateEmail(_email) {
+    // Each time the user types something, we check if the form fields are valid.
+    if (_email.validity.valid) {
+        // In case there is an error message visible, if the field
+        if (_email.classList.contains('is-invalid')){
+            console.log("Remove invalid")
+            _email.classList.remove('is-invalid');
+        }
+        console.log("Valid");
+        _email.classList.add('is-valid');
+
+    } else {
+        console.log("Not valid")
+        _email.classList.add('is-invalid')
+    }
 }
