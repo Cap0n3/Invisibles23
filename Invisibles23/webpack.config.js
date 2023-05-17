0715b578006f88c0ded2d3083ec75151ad49347f
Cap0n3 <cap0n3@protonmail.com>
2023-05-17 12:15:13 +0200

    Installed webpack, changing dev workflow

---
+++ Invisibles23/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mode: 'development',
+    entry: './website/static/website/js/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/website/static/website/js/dist',},
+};
--- Invisibles23/website/static/website/js/modules/common/contactForm.js
+++ Invisibles23/website/static/website/js/modules/common/contactForm.js
@@ -1,28 +1,47 @@
+import JustValidate from 'just-validate';
+
 export function contactForm() {
-    // follow doc : https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
-    const form = document.getElementById("contactForm");
-    const fname = document.getElementById("id_first_name");
-    const lname = document.getElementById("id_last_name");
-    const email = document.getElementById("id_email");
+    const validator = new JustValidate('#contactForm');
 
-    email.addEventListener("input", (event) => {
-        validateEmail(email);
-    });
+    validator.addField('#id_email', [
+        {
+            rule: 'required',
+        },
+        {
+            rule: 'required',
+        },
+        {
+            rule: 'email',
+        },
+    ])
+
 }
 
-function validateEmail(_email) {
-    // Each time the user types something, we check if the form fields are valid.
-    if (_email.validity.valid) {
-        // In case there is an error message visible, if the field
-        if (_email.classList.contains('is-invalid')){
-            console.log("Remove invalid")
-            _email.classList.remove('is-invalid');
-        }
-        console.log("Valid");
-        _email.classList.add('is-valid');
+// JUST IN CASE
 
-    } else {
-        console.log("Not valid")
-        _email.classList.add('is-invalid')
-    }
-}
+//     // follow doc : https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+//     const form = document.getElementById("contactForm");
+//     const fname = document.getElementById("id_first_name");
+//     const lname = document.getElementById("id_last_name");
+//     const email = document.getElementById("id_email");
+
+//     email.addEventListener("input", (event) => {
+//         validateEmail(email);
+//     });
+// }
+
+// function validateEmail(_email) {
+//     // Each time the user types something, we check if the form fields are valid.
+//     if (_email.validity.valid) {
+//         // In case there is an error message visible, if the field
+//         if (_email.classList.contains('is-invalid')){
+//             console.log("Remove invalid")
+//             _email.classList.remove('is-invalid');
+//         }
+//         console.log("Valid");
+//         _email.classList.add('is-valid');
+
+//     } else {
+//         console.log("Not valid")
+//         _email.classList.add('is-invalid')
+//     }
