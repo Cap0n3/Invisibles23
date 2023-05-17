82a9a8d5c196aa0608ad57369dae1c69b9507ceb
Cap0n3 <cap0n3@protonmail.com>
2023-05-17 20:39:45 +0200

    Added form validation

--- Invisibles23/src/js/modules/common/contactForm.js
+++ Invisibles23/src/js/modules/common/contactForm.js
@@ -1,16 +1,60 @@
 import JustValidate from 'just-validate';
 
+const nameRegex = /^[^#+*/()=?°§$£!%_;:<>]+$/;
+const messageRegex = /^[^\[\]{}<>]+$/;
+
 export function contactForm() {
     const validator = new JustValidate('#contactForm', 
         {
-            successFieldCssClass: ['is-valid'],
-            errorFieldCssClass: ['is-invalid'],
-            //errorLabelStyle: { marginTop: '15px', background: 'red', color: '#fff' },
-            errorLabelCssClass: ['invalid-feedback'],
+            successFieldCssClass: ['is-valid'], // CSS class to add when input is valid
+            errorFieldCssClass: ['is-invalid'], // CSS class to add when input is invalid
+            errorLabelCssClass: ['invalid-feedback'], // CSS class to add for error message
         }
       );
 
     validator
+        .addField('#id_first_name', [
+            {
+                rule: "required",
+                errorMessage: "Merci d'indiquer votre prénom",
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                errorMessage: "Votre prénom doit contenir au moins 2 caractères",
+            },
+            {
+                rule: 'maxLength',
+                value: 50,
+                errorMessage: "Votre prénom ne peut pas contenir plus de 50 caractères",
+            },
+            {
+                rule: 'customRegexp',
+                value: nameRegex,
+                errorMessage: "Un caractère invalide a été détecté",
+            },
+        ])
+        .addField('#id_last_name', [
+            {
+                rule: "required",
+                errorMessage: "Merci d'indiquer votre nom",
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                errorMessage: "Votre nom doit contenir au moins 2 caractères",
+            },
+            {
+                rule: 'maxLength',
+                value: 50,
+                errorMessage: "Votre nom ne peut pas contenir plus de 50 caractères",
+            },
+            {
+                rule: 'customRegexp',
+                value: nameRegex,
+                errorMessage: "Un caractère invalide a été détecté",
+            },
+        ])
         .addField('#id_email', [
             {
                 rule: "required",
@@ -18,37 +62,28 @@ export function contactForm() {
             },
             {
                 rule: "email",
-                errorMessage: "Votre adresse email n'est pas valide",
+                errorMessage: "Votre adresse email semble ne pas être valide",
             },
         ])
-
+        .addField('#id_message', [
+            {
+                rule: "required",
+                errorMessage: "Vous avez oublié d'écrire votre message",
+            },
+            {
+                rule: 'minLength',
+                value: 5,
+                errorMessage: "Il semblerait que votre message soit un peu court",
+            },
+            {
+                rule: 'maxLength',
+                value: 10000,
+                errorMessage: "Hum, votre message est un peu trop long (max 10'000 caractères)",
+            },
+            {
+                rule: 'customRegexp',
+                value: messageRegex,
+                errorMessage: "Un caractère invalide a été détecté",
+            },
+        ])
 }
-
-// JUST IN CASE
-
-//     // follow doc : https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
-//     const form = document.getElementById("contactForm");
-//     const fname = document.getElementById("id_first_name");
-//     const lname = document.getElementById("id_last_name");
-//     const email = document.getElementById("id_email");
-
-//     email.addEventListener("input", (event) => {
-//         validateEmail(email);
-//     });
-// }
-
-// function validateEmail(_email) {
-//     // Each time the user types something, we check if the form fields are valid.
-//     if (_email.validity.valid) {
-//         // In case there is an error message visible, if the field
-//         if (_email.classList.contains('is-invalid')){
-//             console.log("Remove invalid")
-//             _email.classList.remove('is-invalid');
-//         }
-//         console.log("Valid");
-//         _email.classList.add('is-valid');
-
-//     } else {
-//         console.log("Not valid")
-//         _email.classList.add('is-invalid')
-//     }
