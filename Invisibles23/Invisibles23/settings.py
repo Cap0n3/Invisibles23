a155c4f7f2ba3b773dcf87ca357d09230e1cf9dd
Cap0n3 <cap0n3@protonmail.com>
2023-07-06 08:41:24 +0200

    Cloudinary through HTTPS

--- Invisibles23/Invisibles23/settings.py
+++ Invisibles23/Invisibles23/settings.py
@@ -164,6 +164,7 @@ cloudinary.config(
     cloud_name = env('CLOUDINARY_CLOUD_NAME'),
     api_key = env('CLOUDINARY_API_KEY'),
     api_secret = env('CLOUDINARY_API_SECRET'),
+    secure = True
 )
 
 # ====== SECURITY ====== #
