d3f5b811f8f29bd5c1becd1d9f203e15a2962cbb
Cap0n3 <cap0n3@protonmail.com>
2023-10-06 18:49:13 +0200

    Membership in prod => mod1

--- Invisibles23/Invisibles23/settings.py
+++ Invisibles23/Invisibles23/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DOMAIN = "www.lesinvisibles.ch"
 
 # Owner & Dev email addresses
-OWNER_EMAIL = "dev.aguillin@gmail.com"
+OWNER_EMAIL = "association@lesinvisibles.ch"
 DEV_EMAIL = "dev.aguillin@gmail.com"
 
 # Quick-start development settings - unsuitable for production
